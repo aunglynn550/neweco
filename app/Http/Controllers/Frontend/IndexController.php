fdacdefd1297327204391a31986731f4ace4a95e
aunglynn550 <aunglynn550@gmail.com>
2023-08-15 22:07:28 +0630

    Product Featured/HotDeals/SpecialDeals/SpecialOffers Show

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -18,7 +18,12 @@ class IndexController extends Controller
         $products = Product::where('status',1)->orderBy('id','DESC')->limit(6)->get();
         $sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
         $categories = Category::orderBy('category_name_en','ASC')->get();
-        return view('frontend.index',compact('categories','sliders','products'));
+
+        $featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
+        $hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(6)->get();
+        $special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(6)->get();
+        $special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(6)->get();
+        return view('frontend.index',compact('categories','sliders','products','featured','hot_deals','special_offer','special_deals'));
     }//End Method
     public function UserLogout(){
         Auth::logout();
